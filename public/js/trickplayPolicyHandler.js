95d86c2a83eadc62ce56a0d8aa9cfa128530186d
Tony Van den Eynde <tony.van.den.eynde@zappware.com>
2022-06-21 15:35:28 +0200

    chromecast

--- public/js/trickplayPolicyHandler.js
+++ public/js/trickplayPolicyHandler.js
@@ -106,6 +106,7 @@ com.zappware.chromecast.trickplayPolicyHandler = (function () {
     if (mode === com.zappware.chromecast.PlaybackMode.LIVETV || (mode === com.zappware.chromecast.PlaybackMode.PLTV && previousMode === com.zappware.chromecast.PlaybackMode.LIVETV)) return true
     const trickplayPolicy = getTrickplayRestrictionPolicy()
     console.log('bugg trickplayPolicy:', trickplayPolicy)
+    getPlaybackMode()
     if (!trickplayPolicy) return true
     const isVod = media && media._playbackMode === com.zappware.chromecast.PlaybackMode.VOD
     if (trickplayPolicy && trickplayPolicy.allow_pause === false) {
