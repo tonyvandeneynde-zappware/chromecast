1b9e11c13c5a8638b67150fe3e497f1677002a26
Tony Van den Eynde <tony.van.den.eynde@zappware.com>
2022-07-20 14:49:05 +0200

    chromecast

---
+++ public/js/error.js
@@ -0,0 +1,25 @@
+/** @license (c) 2017-2019 by Zappware.
+ * All Rights Reserved. No part of this application or any of its contents may be
+ * reproduced, copied, modified or adapted, without the prior written consent of
+ * Zappware.
+ **/
+
+com.zappware.chromecast.Error = {
+    NO_ERROR: "ZCR_ERR_NO_ERROR",
+    UNEXPECTED_EXCEPTION: "ZCR_ERR_UNEXPECTED_EXCEPTION",
+    SESSION_LIMIT_REACHED: "ZCR_ERR_SESSION_LIMIT_REACHED",
+    SESSION_SERVER_ERROR: "ZCR_ERR_SESSION_SERVER_ERROR",
+    SESSION_SERVER_DOWN: "ZCR_ERR_SESSION_SERVER_DOWN",
+    NOT_AUTHORIZED: "ZCR_ERR_NOT_AUTHORIZED",
+    NO_NETWORK: "ZCR_ERR_NO_NETWORK",
+    PLAYBACK: "ZCR_ERR_PLAYBACK",
+    LOAD_FAILED: "ZCR_ERR_LOAD_FAILED",
+    SEEK_FAILED: "ZCR_ERR_SEEK_FAILED",
+    NO_METADATA: "ZCR_ERR_NO_METADATA",
+    EVENT_BLOCKED: "ZCR_ERR_EVENT_BLOCKED",
+    GRAPHQL_ERROR: "ZCR_ERR_GRAPHQL"
+};
+
+com.zappware.chromecast.ErrorMessage = {
+    ZCR_ERR_NO_METADATA: {title: 'activateAppTitle', description: 'activateAppDescription'}
+};
